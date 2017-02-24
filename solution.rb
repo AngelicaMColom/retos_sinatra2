7a99aeed1ea4ad30d0166cb6fe2aa5c70404d7d5
Angélica María D <angelidu@gmail.com>
2017-02-23 19:56:08 -0500

    commit 3

--- solution.rb
+++ solution.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
 
 get '/' do
+  
 end
 get '/maker/:nombre' do
     if params[:nombre] && params[:nombre] != ""
       "<h1>hola #{params[:nombre]}!</h1>"
+    end
 end
 get '/maker/' do
+
     "<h1>hola desconocido!</h1>"
 end
